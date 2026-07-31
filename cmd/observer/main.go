0f39372307b422369efa2693deea70b0b6a52e0b
Ibrahim Wanka <224wankaibrahim@gmail.com>
2026-07-31 11:35:54 +0100

    fix: fix shutdown handler for ring buffer reader

--- cmd/observer/main.go
+++ cmd/observer/main.go
@@ -1,10 +1,11 @@
 package main
 
-//go:generate bpf2go -cc clang -cflags "-O2 -g -Wall -target bpf -D__TARGET_ARCH_x86" TcpConnect ../../bpf/tcp_connect.c -- -I/usr/include/bpf
+//go:generate bpf2go -cc clang -cflags "-O2 -g -Wall -target bpf -D__TARGET_ARCH_x86" TcpTracker ../../bpf/tcp_tracker.c -- -I/usr/include/bpf
 
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"log"
 	"net"
@@ -22,13 +23,13 @@ func main() {
 		log.Fatalf("removing memlock: %v", err)
 	}
 
-	objs := TcpConnectObjects{}
-	if err := LoadTcpConnectObjects(&objs, nil); err != nil {
+	objs := TcpTrackerObjects{}
+	if err := LoadTcpTrackerObjects(&objs, nil); err != nil {
 		log.Fatalf("loading objects: %v", err)
 	}
 	defer objs.Close()
 
-	kp, err := link.Kprobe("tcp_connect", objs.TcpConnectPrograms.TraceTcpConnect, nil)
+	kp, err := link.Kprobe("tcp_connect", objs.TcpTrackerPrograms.TraceTcpConnect, nil)
 	if err != nil {
 		log.Fatalf("attaching kprobe: %v", err)
 	}
@@ -37,7 +38,7 @@ func main() {
 	stopc := make(chan os.Signal, 1)
 	signal.Notify(stopc, syscall.SIGINT, syscall.SIGTERM)
 
-	rd, err := ringbuf.NewReader(objs.TcpConnectMaps.Events)
+	rd, err := ringbuf.NewReader(objs.TcpTrackerMaps.Events)
 	if err != nil {
 		log.Fatalf("opening ring buffer: %v", err)
 	}
@@ -54,32 +55,35 @@ func main() {
 
 	fmt.Println("Listening for TCP connections... Press Ctrl+c to stop")
 
+	go func() {
+		<-stopc
+		rd.Close()
+	}()
+
 	for {
-		select {
-		case <-stopc:
-			fmt.Println("Shutting down.")
-			return
-
-		default:
-			record, err := rd.Read()
-			if err != nil {
-				log.Printf("reading ring buffer: %v", err)
-				continue
+		record, err := rd.Read()
+		if err != nil {
+			if errors.Is(err, ringbuf.ErrClosed) {
+				fmt.Println("Shutting down.")
+				return
 			}
-
-			var event Event
-			if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &event); err != nil {
-				log.Printf("parsing event: %v", err)
-				continue
-			}
-
-			src := net.IP(intToBytes(event.Saddr))
-			dst := net.IP(intToBytes(event.Daddr))
-			comm := string(bytes.TrimRight(event.Comm[:], "\x00"))
-
-			fmt.Printf("PID: %-6d COMM: %-20s SRC: %-20s DST: %s:%d\n",
-				event.Pid, comm, src, dst, event.Dport)
+			log.Printf("reading ring buffer: %v", err)
+			continue
 		}
+
+		var event Event
+		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &event); err != nil {
+			log.Printf("parsing event: %v", err)
+			continue
+		}
+
+		src := net.IP(intToBytes(event.Saddr))
+		dst := net.IP(intToBytes(event.Daddr))
+		comm := string(bytes.TrimRight(event.Comm[:], "\x00"))
+
+		fmt.Printf("PID: %-6d COMM: %-20s SRC: %-20s DST: %s:%d\n",
+			event.Pid, comm, src, dst, event.Dport)
+
 	}
 
 }
